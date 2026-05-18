5b25e95e624f5ec5e4553bd3c92994d69259fd99
buhahahahaa <119365083+itsMe-Wario@users.noreply.github.com>
2026-05-18 18:05:44 +0545

    fixed experimental

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
     serverComponentsExternalPackages: ["mongoose"],
-  },
 };
 
 export default nextConfig;
